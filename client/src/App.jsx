10632c20ee701601167952905830b57e4d360c1c
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-04-26 13:17:52 -0400

    Web interface

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+
+function App() {
+    let pokemon1 = "";
+    let pokemon2 = "";
+
+    return (
+        <>
+            <div className="pokemon" id="pokemon1">
+                Pokemon: {pokemon1}
+            </div>
+
+            <div className="pokemon" id="pokemon2">
+                Pokemon: {pokemon2}
+            </div>
+        </>
+    )
+}
+
+export default App
