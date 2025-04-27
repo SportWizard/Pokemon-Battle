51f5c4ad5da63f54bd93dac6e0cba2ac6f5770fd
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-04-27 10:51:05 -0400

    Update database script

--- server/database/pokemon_db.sql
+++ server/database/pokemon_db.sql
@@ -3,23 +3,23 @@ CREATE SCHEMA IF NOT EXISTS pokemon_data;
 USE pokemon_data;
 
 CREATE TABLE IF NOT EXISTS pokemon (
-    pokemon_id INT NOT NULL,
-    pokemon_name VARCHAR(20),
+    pokemon_id INT,
+    pokemon_name VARCHAR(20) UNIQUE,
     hp INT NOT NULL,
     PRIMARY KEY (pokemon_id)
 );
 
 CREATE TABLE IF NOT EXISTS move (
-    move_id INT NOT NULL,
-    move_name VARCHAR(20),
+    move_id INT,
+    move_name VARCHAR(20) UNIQUE,
     power INT NOT NULL,
     accuracy FLOAT NOT NULL,
     PRIMARY KEY (move_id)
 );
 
 CREATE TABLE IF NOT EXISTS has (
-    pokemon_id INT NOT NULL,
-    move_id INT NOT NULL,
+    pokemon_id INT,
+    move_id INT,
     PRIMARY KEY (pokemon_id, move_id),
     FOREIGN KEY (pokemon_id) REFERENCES pokemon (pokemon_id) ON DELETE CASCADE,
     FOREIGN KEY (move_id) REFERENCES move (move_id) ON DELETE CASCADE
